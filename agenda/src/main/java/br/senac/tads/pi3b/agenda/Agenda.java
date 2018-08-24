edf61ebd2fa13e26a1d9ae301238ee7466770e6b
Fernando Tsuda <ftsuda.senac@gmail.com>
2018-08-23 22:08:34 -0300

    Versão inicial

---
+++ agenda/src/main/java/br/senac/tads/pi3b/agenda/Agenda.java
@@ -0,0 +1,81 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Senac TADS PI3-B.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.senac.tads.pi3b.agenda;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+public class Agenda {
+
+    private Connection obterConexao()
+            throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+
+        // Passo 1: Registrar driver JDBC
+        Class.forName("com.mysql.jdbc.Driver");
+        // Passo 2: Obter a conexao
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/agendabd",
+                "root",
+                "");
+        return conn;
+    }
+
+    public void executar() {
+        String querySql
+                = "SELECT ID, NOME, DTNASCIMENTO FROM PESSOA";
+
+        try (
+                Connection conn = obterConexao();
+                PreparedStatement stmt = conn.prepareStatement(querySql);
+                ResultSet resultados = stmt.executeQuery()) {
+
+            while (resultados.next()) {
+                long id = resultados.getLong("ID");
+                String nome = resultados.getString("NOME");
+                Date dtNascimento = resultados.getDate("DTNASCIMENTO");
+                System.out.println(id + " " + nome + " " + dtNascimento);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda();
+        agenda.executar();
+    }
+}
